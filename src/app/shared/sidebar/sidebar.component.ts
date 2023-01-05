e7d309c1d1d1f4fa6e034e5a18dd18419037c38a
Francisco Quintero <frankjrdev@gmail.com>
2023-01-04 21:32:08 -0400

    ajustes footer y sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../../modules/home/services/categorie/categories.service';
+import { Category } from '../../interfaces/category.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+
+  listCategories!: Category[];
+
+  constructor(private categoriesServices: CategoriesService) { }
+
+  ngOnInit(): void {
+    this.loadAllCategories();
+  }
+
+  async loadAllCategories(): Promise<any> {
+    this.listCategories = await firstValueFrom(this.categoriesServices.getAllCategories$());
+    console.log(this.listCategories);
+
+  }
 
 }
